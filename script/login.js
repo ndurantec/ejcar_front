d14f89fe9e9a0d0b79e650cc0ead77142a213fdf
IgorEffko <igor.vendruscolo@escola.pr.gov.br>
2025-10-20 10:10:43 -0300

    fiz a validação do login

--- script/login.js
+++ script/login.js
@@ -10,4 +10,20 @@ botaoEntrar.addEventListener('click', function(evento) {
     console.log("Usuário:", usuario);
     console.log("Senha:", senha);
     console.log("Email:", email);
+
+  if (usuario === '') {
+     document.getElementById('erro-usuario').textContent = '';
+       
+  }
+
+ 
+  if (senha === '') {
+     document.getElementById('erro-senha').textContent = 'Preencha a senha!';
+  }
+
+ 
+  if (email === '') {
+     document.getElementById('erro-email').textContent = 'Preencha o email!';
+  } 
+
 });
