5437bc862f24249f36fd734de7476ace71d5d433
joaoTRYHARD <joao.pietro.santos@escola.pr.gov.br>
2025-10-27 10:24:29 -0300

    validaod

--- script/agendamento.js
+++ script/agendamento.js
@@ -26,6 +26,30 @@ function cadastrar() {
 
 
 function salvarAgendamento() {
+
+     const dataEntrada = document.getElementById("entradaVeiculo").value;
+
+    const dataSaida = document.getElementById("saidaVeiculo").value;
+
+    const seguradoraSim = document.getElementById("seguradoraSim").value;
+
+    const seguradoraNao= document.getElementById("seguradoraNao").value;
+
+
+            if(dataEntrada == ""){
+                alert("Você precisa preencher a entrada do veiculo");
+            }
+
+            if(dataSaida == ""){
+                alert("Você precisa preencher a saida do veiculo");
+            }
+
+             if(seguradoraSim, seguradoraNao == ""){
+                alert("Você precisa preencher o campo endereco");
+            }
+
+
+                alert(dataEntrada + " - " + dataSaida + " - " + seguradoraSim + " - " + seguradoraNao )
    
     // Envia os dados via fetch
     fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
