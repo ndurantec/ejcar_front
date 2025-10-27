ced7170192add7c097c5850e65b651e1f6c961a1
EduardaMary <ossovski.maria@escola.pr.gov.br>
2025-10-27 10:27:01 -0300

    feito

--- script/servico.js
+++ script/servico.js
@@ -51,6 +51,28 @@ function consultar() {
 function salvar() {
    
 
+   let funcionario = document.getElementById("funcionario").value.trim();
+    let dataServico = document.getElementById("data").value;
+    let realizado = document.querySelector('input[name="realizado"]:checked');
+
+    if (funcionario === "") {
+      alert("Você precisa preencher o nome do funcionário");
+      return;
+    }
+
+    if (dataServico === "") {
+      alert("Por favor, selecione uma data");
+      return;
+    }
+
+    if (!realizado) {
+      alert("Por favor, selecione se o serviço foi realizado");
+      return;
+    }
+
+    alert("Sucesso! Operação concluída.");
+
+    form.reset();
 
     // Envia os dados via fetch
     fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
