feee7d49207c4c0f1669d6c24b49c27cd3e24469
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2025-10-31 07:56:13 -0300

    ejcar

--- script/orcamento.js
+++ script/orcamento.js
@@ -23,4 +23,82 @@ function finalizar(){
         if(mao_de_obra == ""){
                 alert("Você precisa preencher o campo mao de obra");
         }
+     
+       
+
+}  
+function cadastrarorcamento() {
+
+       const chassi = document.getElementById("chassi").value;
+
+       const placa = document.getElementById("placa").value;
+    
+       const produto = document.getElementById("produto").value;
+
+       const mao_de_obra = document.getElementById("mao_de_obra").value;
+       alert(chassi + " - " + placa + " - " + produto + " - " + mao_de_obra);
+
+        if(chassi == ""){
+                alert("Você precisa preencher o campo chassi");
+        }
+
+        if(placa == ""){
+                alert("Você precisa preencher o campo placa");
+        }
+
+        if(produto == ""){
+                alert("Você precisa preencher o campo produto");
+        }
+     
+        if(mao_de_obra == ""){
+                alert("Você precisa preencher o campo mao de obra");
+        }
+        
+      fetch("http://localhost:8080/orcamento/cadorca", 
+       
+      ).then(response => {
+           
+      }).then(data => {
+       
+      }).catch(error => {
+       
+      });
 }
+
+function consultarorcamento(){
+              fetch("http://localhost:8080/orcamento/{id}", 
+       
+      ).then(response => {
+           
+      }).then(data => {
+       
+      }).catch(error => {
+       
+      });
+}
+
+function deletar(){
+              fetch("http://localhost:8080/orcamento/{id}", 
+       
+      ).then(response => {
+           
+      }).then(data => {
+       
+      }).catch(error => {
+       
+      });
+}
+
+function atualizar(){
+              fetch("http://localhost:8080/orcamento/{id}", 
+       
+      ).then(response => {
+           
+      }).then(data => {
+       
+      }).catch(error => {
+       
+      });
+}
+
+ 
